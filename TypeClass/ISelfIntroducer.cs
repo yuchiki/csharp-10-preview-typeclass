08e72466f31061d325430e24d3adfc3b5e5564ee
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-05-20 13:11:11 +0900

    iSelfIntroducer

---
+++ TypeClass/ISelfIntroducer.cs
@@ -0,0 +1,12 @@
+﻿namespace TypeClass;
+
+public interface ISelfIntroducer
+{
+    public abstract static string IntroduceTypeItself();
+}
+
+
+public class HappyClass : ISelfIntroducer
+{
+    public static string IntroduceTypeItself() => "I'm a happy lucky class!";
+}
